d0d89c8f978a0a76d42e6ae7332868499f6512e3
Ridwan <ridwan0abdullahi@gmail.com>
2023-03-30 15:11:55 -0500

    imports

--- podcast_summary.py
+++ podcast_summary.py
@@ -1,1 +1,16 @@
-msg = "testing"
+import os
+import json
+import requests
+import xmltodict
+
+from airflow.decorators import dag, task
+import pendulum
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.sqlite.hooks.sqlite import SqliteHook
+
+from vosk import Model, KaldiRecognizer
+from pydub import AudioSegment
+
+PODCAST_URL = "https://www.marketplace.org/feed/podcast/marketplace/"
+EPISODE_FOLDER = "episodes"
+FRAME_RATE = 16000
